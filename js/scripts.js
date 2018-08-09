ea9e583a4b78438622901579259f9be03e675767
Chris Crow <chrismcrow@gmail.com>
2018-08-08 23:38:43 -0700

    fully functions with updates to layout

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@
 $(document).ready(function() {
   $("#newcontact").submit(function(event) {
     event.preventDefault();
-    var contactName = $("input#name").val();
+    var contactName = $("input#lastname").val() + ", " + $("input#firstname").val();
     var contactPage =
-      "Name: " + $("input#name").val() + "<br>" +
+      "Name: " + $("input#firstname").val() + " " + $("input#lastname").val() + "<br>" +
       "Phone Number: " + $("input#phone").val() + "<br>" +
       "Email: " + $("input#email").val() + "<br>" +
       "Adress: " + $("input#adress1").val() + "<br>" +
       "City/State/Zip: " + $("input#address2").val();
-      //currently when we create a second contact, it's adding it twice. Once to the top level .addressbook, and once to the new .clickable div inside of that one.
+
     $(".addressbook").append("<div class='clickable'>" + "</div>");
     $(".clickable:last-child").append("<p>" + contactName + "</p>");
     $(".clickable p:first-child").addClass("front");
